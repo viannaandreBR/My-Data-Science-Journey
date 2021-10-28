6308d5c16289e2c62a3450f82d3892f92a3e860d
Andre Souza <viannaandresouza@gmail.com>
2021-10-28 14:31:33 -0300

    Update Machine Learning Classification2

---
+++ 7-Machine-Learning/Demo-Image-Classification-Android/app/src/main/java/com/example/imageclassification/MainActivity.kt
@@ -0,0 +1,143 @@
+package com.example.imageclassification
+
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.preference.PreferenceManager
+import android.provider.MediaStore
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import com.example.imageclassification.ml.MobilenetV110224Quant
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+import java.util.jar.Manifest
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var select_image_button : Button
+    lateinit var make_prediction : Button
+    lateinit var img_view : ImageView
+    lateinit var text_view : TextView
+    lateinit var bitmap: Bitmap
+    lateinit var camerabtn : Button
+
+    public fun checkandGetpermissions(){
+        if(checkSelfPermission(android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED){
+            requestPermissions(arrayOf(android.Manifest.permission.CAMERA), 100)
+        }
+        else{
+            Toast.makeText(this, "Camera permission granted", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == 100){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                Toast.makeText(this, "Camera permission granted", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        select_image_button = findViewById(R.id.button)
+        make_prediction = findViewById(R.id.button2)
+        img_view = findViewById(R.id.imageView2)
+        text_view = findViewById(R.id.textView)
+        camerabtn = findViewById<Button>(R.id.camerabtn)
+
+        // handling permissions
+        checkandGetpermissions()
+
+        val labels = application.assets.open("labels.txt").bufferedReader().use { it.readText() }.split("\n")
+
+        select_image_button.setOnClickListener(View.OnClickListener {
+            Log.d("mssg", "button pressed")
+            var intent : Intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+
+            startActivityForResult(intent, 250)
+        })
+
+        make_prediction.setOnClickListener(View.OnClickListener {
+            var resized = Bitmap.createScaledBitmap(bitmap, 224, 224, true)
+            val model = MobilenetV110224Quant.newInstance(this)
+
+            var tbuffer = TensorImage.fromBitmap(resized)
+            var byteBuffer = tbuffer.buffer
+
+// Creates inputs for reference.
+            val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 224, 224, 3), DataType.UINT8)
+            inputFeature0.loadBuffer(byteBuffer)
+
+// Runs model inference and gets result.
+            val outputs = model.process(inputFeature0)
+            val outputFeature0 = outputs.outputFeature0AsTensorBuffer
+
+            var max = getMax(outputFeature0.floatArray)
+
+            text_view.setText(labels[max])
+
+// Releases model resources if no longer used.
+            model.close()
+        })
+
+        camerabtn.setOnClickListener(View.OnClickListener {
+            var camera : Intent = Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(camera, 200)
+        })
+
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == 250){
+            img_view.setImageURI(data?.data)
+
+            var uri : Uri ?= data?.data
+            bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, uri)
+        }
+        else if(requestCode == 200 && resultCode == Activity.RESULT_OK){
+            bitmap = data?.extras?.get("data") as Bitmap
+            img_view.setImageBitmap(bitmap)
+        }
+
+    }
+
+    fun getMax(arr:FloatArray) : Int{
+        var ind = 0;
+        var min = 0.0f;
+
+        for(i in 0..1000)
+        {
+            if(arr[i] > min)
+            {
+                min = arr[i]
+                ind = i;
+            }
+        }
+        return ind
+    }
+}
